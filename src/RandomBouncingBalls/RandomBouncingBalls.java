6a6231c6a44d924faad7ab444837fba2b617af62
FS <florin.schwappach@ur.de>
2019-10-16 10:55:54 +0200

    Kommentare deutsch

--- src/RandomBouncingBalls/RandomBouncingBalls.java
+++ src/RandomBouncingBalls/RandomBouncingBalls.java
@@ -6,14 +6,15 @@ import de.ur.mi.oop.colors.Colors;
 
 public class RandomBouncingBalls extends GraphicsApp {
 
-    /* Private constants */
+    /* Private Konstanten */
     private static final int CANVAS_HEIGHT = 800;
     private static final int CANVAS_WIDTH = 800;
     private static final int FRAME_RATE = 60;
     private static final Color BACKGROUND_COLOR = Colors.WHITE;
 
     /*
-     * This method is called once when the program is started.
+     * Die initialize-Methode wird einmalig zum Start des Programms
+     * aufgerufen.
      */
 
     @Override
@@ -22,7 +23,8 @@ public class RandomBouncingBalls extends GraphicsApp {
     }
 
     /*
-     * This method is called repeatedly while the program is running.
+     * Die draw-Methode wird so lange wiederholt aufgerufen, bis das Programm
+     * beendet wird.
      */
 
     @Override
