2836e74665b86fbf1d266ddb94de7dbffdcb12c2
FS <florin.schwappach@ur.de>
2019-11-12 13:46:50 +0100

    Updated GA, changed second task

--- src/CircleRain/CircleRain.java
+++ src/CircleRain/CircleRain.java
@@ -3,6 +3,9 @@ package CircleRain;
 import de.ur.mi.oop.app.GraphicsApp;
 import de.ur.mi.oop.colors.Color;
 import de.ur.mi.oop.colors.Colors;
+import de.ur.mi.oop.graphics.Circle;
+
+import java.util.Random;
 
 public class CircleRain extends GraphicsApp {
 
@@ -11,11 +14,16 @@ public class CircleRain extends GraphicsApp {
     private static final int CANVAS_WIDTH = 800;
     private static final int FRAME_RATE = 60;
     private static final Color BACKGROUND_COLOR = Colors.WHITE;
-    private static final float MIN_BALL_VELOCITY = 2;
-    private static final float MAX_BALL_VELOCITY = 10;
 
-    private RandomBall ballOne;
-    private RandomBall ballTwo;
+    private static final int CIRCLE_COUNT = 50;
+    private static final int CIRCLE_WIDTH = CANVAS_WIDTH/CIRCLE_COUNT;
+    private static final int CIRCLE_RADIUS = CIRCLE_WIDTH/2;
+    private static final int MAX_SPEED = 10;
+
+    private static final Random rand = new Random();
+
+    private Circle[] circles;
+    private float[] circleSpeeds;
 
     /*
      * Die initialize-Methode wird einmalig zum Start des Programms
@@ -25,17 +33,7 @@ public class CircleRain extends GraphicsApp {
     @Override
     public void initialize() {
         setupCanvas();
-        setupRandomBalls();
-    }
-
-    private void setupRandomBalls() {
-        ballOne = new RandomBall(CANVAS_WIDTH, CANVAS_HEIGHT, MIN_BALL_VELOCITY, MAX_BALL_VELOCITY);
-        ballTwo = new RandomBall(CANVAS_WIDTH, CANVAS_HEIGHT, MIN_BALL_VELOCITY, MAX_BALL_VELOCITY);
-    }
-
-    private void setupCanvas() {
-        setCanvasSize(CANVAS_WIDTH, CANVAS_HEIGHT);
-        setFrameRate(FRAME_RATE);
+        setupCirclesAndSpeeds();
     }
 
     /*
@@ -46,17 +44,49 @@ public class CircleRain extends GraphicsApp {
     @Override
     public void draw() {
         drawBackground(BACKGROUND_COLOR);
-        drawBalls();
+        drawCircles();
     }
 
-    private void drawBalls() {
-        drawBall(ballOne);
-        drawBall(ballTwo);
+    /*
+     * In dieser Methode wird die Position aller Kreise aktualisiert
+     * und die Kreise werden gezeichnet.
+     */
+    private void drawCircles() {
+        for (int i = 0; i < CIRCLE_COUNT; i++) {
+            for (int j = 0; j < 10; j++) {
+                updateCircle(circles[i], circleSpeeds[i]);
+                circles[i].draw();
+            }
+        }
     }
 
-    private void drawBall(RandomBall ball) {
-        ball.checkWallCollision(CANVAS_WIDTH, CANVAS_HEIGHT);
-        ball.update();
-        ball.draw();
+    /*
+     * Bei der Kreisaktualisierung wird überprüft, ob der Kreis am unteren Rand
+     * anstößt. Wenn dies der Fall ist, wird er wieder auf seine Startposition gesetzt.
+     */
+    private void updateCircle(Circle circle, float speed) {
+        circle.move(0, speed);
+        if (circle.getYPos() > CANVAS_HEIGHT) {
+            circle.setYPos(CIRCLE_RADIUS);
+        }
+    }
+
+    /*
+     * In dieser Methode werden beide Arrays instanziiert und mit Objekten gefüllt.
+     * Die Geschwindigkeit wird zufällig festgelegt.
+     */
+    private void setupCirclesAndSpeeds() {
+        circleSpeeds = new float[CIRCLE_COUNT];
+        circles = new Circle[CIRCLE_COUNT];
+
+        for (int i = 0; i < CIRCLE_COUNT; i++) {
+            circles[i] = new Circle(CIRCLE_RADIUS + i * CIRCLE_WIDTH, CIRCLE_RADIUS, CIRCLE_RADIUS, Colors.getRandomColor());
+            circleSpeeds[i] = rand.nextFloat() * MAX_SPEED;
+        }
+    }
+
+    private void setupCanvas() {
+        setCanvasSize(CANVAS_WIDTH, CANVAS_HEIGHT);
+        setFrameRate(FRAME_RATE);
     }
 }
